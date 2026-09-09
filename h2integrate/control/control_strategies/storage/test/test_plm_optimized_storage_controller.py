48be49a61a382c9bfdd5a1c2389de951d474569b
Sanjana Vijayshankar <59975769+vijay092@users.noreply.github.com>
2026-09-09 12:04:46 -0600

    Update h2integrate/control/control_strategies/storage/test/test_plm_optimized_storage_controller.py

--- h2integrate/control/control_strategies/storage/test/test_plm_optimized_storage_controller.py
+++ h2integrate/control/control_strategies/storage/test/test_plm_optimized_storage_controller.py
@@ -698,7 +698,7 @@ def test_plm_history(subtests, om_plant_config, om_tech_config):
         ]
     )
 
-    active          = p_discharge_coop_history > 0.5
+    active = p_discharge_coop_history > 0.5
     expected_active = expected_history > 0.5
     
     with subtests.test("total coop discharge energy matches"):
